19500c1dfe1b09261487c65fd85fb16a1f5cad43
Krisel1 <kriselur@gmail.com>
2024-06-26 18:48:00 +0200

    Verification code MagiCardtest

--- src/test/java/org/example/MagicCardTest.java
+++ src/test/java/org/example/MagicCardTest.java
@@ -2,13 +2,47 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MagicCardTest {
     @Test
-    void calculatePriceForMagicCards_blue() {
+    void calculate_Age_For_MagicCards_Blue() {
+
+        MagicCard magicCard = new MagicCard("Magic: The Gathering", 10, "blue");
+
+        assertEquals(magicCard.calculatePrice(), 2.5);
+
+    }
+
+    @Test
+    void calculate_Age_For_MagicCards_Red() {
+
+        MagicCard magicCard = new MagicCard("Magic: The Gathering", 10, "red");
+
+        assertEquals(magicCard.calculatePrice(), 1.75);
+
+    }
+
+    @Test
+    void calculate_Age_For_MagicCards_Green() {
+
+        MagicCard magicCard = new MagicCard("Magic: The Gathering", 20, "green");
+
+        assertEquals(magicCard.calculatePrice(), 5.28);
+
+    }
+
+    @Test
+    void calculate_Age_For_MagicCards_Black() {
+
+        MagicCard magicCard = new MagicCard("Magic: The Gathering", 20, "black");
+
+        assertEquals(magicCard.calculatePrice(), 8.16);
+
+    }
+
+    @Test
+    void calculate_Price_For_MagicCards_blue() {
 
         MagicCard magicCard = new MagicCard("Magic: The Gathering", null, "blue");
 
@@ -43,7 +77,7 @@ public class MagicCardTest {
     }
 
     @Test
-    void calculate_Price_For_Magic_Cards_Brown() {
+    void calculate_Price_For_MagicCards_Brown() {
 
         MagicCard magicCard = new MagicCard("Magic: The Gathering", null, "brown");
 
@@ -52,10 +86,11 @@ public class MagicCardTest {
     }
 
     @Test
-    void calculatePriceForMagicCards_blackLotus() {
+    void calculate_Price_For_MagicCards_blackLotus() {
         MagicCard magicCard = new MagicCard("Magic: The Gathering - Black Lotus", null, null);
 
         assertEquals(magicCard.calculatePrice(), 40000.0);
 
     }
+
 }
